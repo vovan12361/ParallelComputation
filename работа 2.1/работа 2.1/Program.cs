32f45b86ff9ea1e963a5ecd451df1959438ff572
vovan12361 <vovan12361@gmail.com>
2024-12-06 17:42:22 +0300

    Added 2 works

---
+++ работа 2.1/работа 2.1/Program.cs
@@ -0,0 +1,140 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+class Program
+{
+    static int[] arr;
+    static int[] a, b;
+
+    static void Circ(object o)
+    {
+        int[] args = (int[])o;
+        int iThread = args[0];
+        int M = args[1];
+        for (int i = iThread; i < b.Length; i += M)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                b[i] = a[i] / 100;
+            }
+        }
+    }
+    static void DivideBy(int[] arr, int k, int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            for (int dif = 0; dif < 30000; dif++)
+            {
+                arr[i] = arr[i] / k;
+            }
+        }
+    }
+    static void LastTask()
+    {
+        int[] th = new int[] { 2, 3, 4, 5, 6, 8, 10, 12, System.Environment.ProcessorCount };
+        Console.WriteLine("Всего потоков в системе {0}", System.Environment.ProcessorCount);
+        Console.WriteLine("N\tM\tTime");
+        TimeSpan ts;
+        double minPar = double.MaxValue;
+        for (int n = 10; n <= 100000; n = n * 10)
+        {
+            a = new int[n];
+            b = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = i + 1;
+                b[i] = 0;
+            }
+            
+            foreach (int m in th)
+            {
+                Thread[] threads = new Thread[m];
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                for (int i = 0; i < m; i++)
+                {
+                    threads[i] = new Thread(Circ);
+                    threads[i].Start(new int[] { i, m });
+                }
+
+                for (int i = 0; i < m; i++)
+                {
+                    threads[i].Join();
+                }
+
+                sw.Stop();
+                ts = sw.Elapsed;
+                if (ts.TotalMilliseconds < minPar) minPar = ts.TotalMilliseconds;
+                Console.WriteLine("{0}\t{1}\t{2}", n, m, ts.TotalMilliseconds);
+                
+            }
+            Console.WriteLine("Min Par:\t\t{0}", minPar);
+            minPar = double.MaxValue;
+            Console.WriteLine();
+        }
+        
+    }
+    static void Main()
+    {
+        int[] th = new int[] { 2, 3, 4, 5, 6, 8, 10, 12, System.Environment.ProcessorCount };
+        int k = 100;
+        double minDiap = double.MaxValue;
+        Console.WriteLine("Всего потоков в системе {0}", System.Environment.ProcessorCount);
+        Console.WriteLine("N\tM\tTime");
+        for (int n = 10; n <= 100000; n = n * 10)
+        {
+            arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            DivideBy(arr, k, 0, n-1);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Console.Write("{0}\t1\t{1}\n", n, ts.TotalMilliseconds);
+            foreach(int m in th)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = i + 1;
+                }
+                Thread[] threads = new Thread[(int)m];
+                int range = (int)Math.Ceiling((double)n / m);
+                Stopwatch sw1 = new Stopwatch();
+                sw1.Start();
+                for (int i = 0; i < m; i++)
+                {
+                    int start = i * range;
+                    int end = start + range - 1;
+                    if (start > n - 1)
+                        break;
+                    if (end > n - 1)
+                        end = n - 1;
+                    threads[i] = new Thread(() => { DivideBy(arr, k, start, end); });
+                    
+                }
+                for(int i = 0; i < m; i++)
+                {
+                    if (threads[i] != null) threads[i].Start();
+                }
+                for(int i = 0; i < m; i++)
+                {
+                    if (threads[i] != null)  threads[i].Join();
+                }
+                sw1.Stop();
+                ts = sw1.Elapsed;
+                if (ts.TotalMilliseconds < minDiap) minDiap = ts.TotalMilliseconds;
+                Console.Write("{0}\t{1}\t{2}\n", n, m, ts.TotalMilliseconds);
+                
+            }
+            Console.WriteLine("Min Diap:\t\t{0}", minDiap);
+            minDiap = double.MaxValue;
+            Console.WriteLine();
+        }
+        LastTask();
+    }
+    
+}
---
+++ работа 2.2/работа 2.2/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        const int N = 1000000;
+        static List<int> basePrime = new List<int>();
+        static int[] Nums = new int[N + 1];
+        static int M = 4;
+        static int cnt;
+        static int current_index = 0;
+        static void Init()
+        {
+            for (int i = 2; i < Math.Sqrt(N); i++)
+            {
+                if (Nums[i] == 0)
+                {
+                    for (int j = i + 1; j < Math.Sqrt(N); j++)
+                        if (j % i == 0)
+                            Nums[j] = 1;
+                    basePrime.Add(i);   
+                }
+            }
+        }
+        //Модифицированный последовательный алгоритм поиска
+        static void Sequential()
+        {
+            Init();
+            for (int i = (int)(Math.Sqrt(N)); i < N + 1; i++)
+                foreach (var item in basePrime)
+                {
+                    if (i % item == 0)
+                        Nums[i] = 1;
+                }
+        }
+        // декомпозиция по данным
+        static void Parallel1() 
+        {
+            Init();
+            Thread[] arrThr = new Thread[M];
+            for (int i = 0; i < M; i++)
+            {
+
+                arrThr[i] = new Thread(ParalellThr1);
+                arrThr[i].Start(i);
+            }
+            for (int i = 0; i < M; i++)
+                arrThr[i].Join();
+
+        }
+        static void ParalellThr1(object obj)
+        {
+            int idx = (int)obj;
+            int end;
+            int Sqrt = (int)Math.Sqrt(N);
+            cnt = (N - Sqrt) / M;
+            int start = Sqrt + cnt * idx;
+            if (idx == M - 1) end = N + 1;
+            else end = start + cnt;
+            for (int i = start; i < end; i++)
+                foreach (var item in basePrime)
+                {
+                    if (i % item == 0)
+                        Nums[i] = 1;
+                }
+        }
+
+        //декомпозиция набора простых чисел
+        static void Parallel2()
+        {
+            Init();
+            Thread[] arrThr = new Thread[M];
+            for (int i = 0; i < M; i++)
+            {
+
+                arrThr[i] = new Thread(ParalellThr2);
+                arrThr[i].Start(i);
+            }
+            for (int i = 0; i < M; i++)
+                arrThr[i].Join();
+
+
+        }
+        private static void ParalellThr2(object obj)
+        {
+            int idx = (int)obj;
+            int end;
+            int Sqrt = (int)Math.Sqrt(N);
+            int Len = basePrime.Count;
+            cnt = Len / M;
+            int start = cnt * idx;
+            if (idx == M - 1) end = Len;
+            else end = start + cnt;
+            for (int i = Sqrt; i < N + 1; i++)
+                for (int j = start; j < end; j++)
+                {
+                    if (i % basePrime[j] == 0)
+                        Nums[i] = 1;
+                }
+        }
+        // применение пула потоков
+        static void Parallel3()
+        {
+            Init();
+            int Len = basePrime.Count;
+            CountdownEvent events = new CountdownEvent(Len);
+            for (int i = 0; i < Len; i++)
+            {
+                ThreadPool.QueueUserWorkItem(ParalellFunc3, new object[] { basePrime[i], events });
+            }
+            events.Wait();
+
+        }
+        // обработка всех чисел диапазона на разложимость простому числу basePrime
+        private static void ParalellFunc3(object obj)
+        {
+            int Sqrt = (int)Math.Sqrt(N);
+            int prime = (int)((object[])obj)[0];
+            CountdownEvent ev = ((object[])obj)[1] as CountdownEvent;
+            for (int i = Sqrt; i < N + 1; i++)
+                if (i % prime == 0)
+                    Nums[i] = 1;
+            ev.Signal();
+        }
+
+        //последовательный перебор простых чисел
+        static void Parallel4()
+        {
+            Init();
+            Thread[] arrThr = new Thread[M];
+            for (int i = 0; i < M; i++)
+            {
+
+                arrThr[i] = new Thread(ParallelFunc4);
+                arrThr[i].Start();
+            }
+            for (int i = 0; i < M; i++)
+                arrThr[i].Join();
+        }
+        private static void ParallelFunc4()
+        {
+            int current_prime;
+            int Len = basePrime.Count;
+            while (true)
+            {
+                if (current_index >= Len)
+                    break;
+                lock ("Critical")
+                {
+                    current_prime = basePrime[current_index];
+                    current_index++;
+                }
+                for (int i = (int)Math.Sqrt(N); i < N + 1; i++)
+                    if (i % current_prime == 0)
+                        Nums[i] = 1;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            DateTime Start, End;
+            Start = DateTime.Now;
+            for (int i = 0; i < 10; i++)
+            {
+                Sequential();
+            }
+            End = DateTime.Now;
+            Console.WriteLine(((End - Start).TotalMilliseconds) + " ms");
+            Start = DateTime.Now;
+            for (int i = 0; i < 10; i++)
+            {
+                Parallel1();
+            }
+            End = DateTime.Now;
+            Console.WriteLine(((End - Start).TotalMilliseconds) + " ms");
+
+            Start = DateTime.Now;
+            for (int i = 0; i < 10; i++)
+            {
+                Parallel2();
+            }
+            End = DateTime.Now;
+            Console.WriteLine(((End - Start).TotalMilliseconds) + " ms");
+
+            Start = DateTime.Now;
+            for (int i = 0; i < 10; i++)
+            {
+                Parallel3();
+            }
+            End = DateTime.Now;
+            Console.WriteLine(((End - Start).TotalMilliseconds) + " ms");
+
+            Start = DateTime.Now;
+            for (int i = 0; i < 10; i++)
+            {
+                Parallel4();
+            }
+            End = DateTime.Now;
+            Console.WriteLine(((End - Start).TotalMilliseconds) + " ms");
+        }
+    }
